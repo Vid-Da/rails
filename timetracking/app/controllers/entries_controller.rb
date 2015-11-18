14ce3b6e90f35d252cce2c5054962cc9165c465a
Vid-Da <d.encinas.t@gmail.com>
2015-11-18 17:30:12 +0100

    update entries

--- timetracking/app/controllers/entries_controller.rb
+++ timetracking/app/controllers/entries_controller.rb
@@ -25,6 +25,22 @@ class EntriesController < ApplicationController
 		end
 	end
 
+	def edit
+		@project = Project.find(params[:project_id])
+		@entry = @project.entries.find(params[:id])
+	end
+
+	def update
+		@project = Project.find(params[:project_id])
+		@entry = @project.entries.find(params[:id])
+
+		if @entry.update_attributes(entry_params)
+			redirect_to action: 'index', controller: 'entries', project_id: @project.id
+		else
+			render 'edit'
+		end
+	end
+
 	private
 	
 	def entry_params
--- timetracking/config/routes.rb
+++ timetracking/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "/" => "site#home"
 
   resources :projects, only: [:index, :show] do
-    resources :entries, only: [:index, :new, :create]
+    resources :entries, only: [:index, :new, :create, :edit, :update]
   end
 
 ## lo anterior equivale a todo esto y mas
