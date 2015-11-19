d155ee8209e776098c947cb1999214570d1b8c26
Vid-Da <d.encinas.t@gmail.com>
2015-11-19 09:36:47 +0100

    method destroy added

--- timetracking/app/controllers/entries_controller.rb
+++ timetracking/app/controllers/entries_controller.rb
@@ -41,6 +41,14 @@ class EntriesController < ApplicationController
 		end
 	end
 
+	def destroy
+		@project = Project.find(params[:project_id])
+		@entry = @project.entries.find(params[:id])
+		
+		@entry.destroy
+		redirect_to action: 'index', controller: 'entries', project_id: @project
+	end
+
 	private
 	
 	def entry_params
